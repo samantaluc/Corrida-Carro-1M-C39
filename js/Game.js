c52a2e40710cd1aa8edb71d3dd82383f58a24698
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-03-23 18:42:07 -0300

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -1,35 +1,28 @@
 class Game {
   constructor() {}
-  //BP
-  getState() {
+  getState() { //LÊ QUAL O ESTADO DE JOGO
     var gameStateRef = database.ref("gameState");
     gameStateRef.on("value", function(data) {
       gameState = data.val();
     });
   }
-  //BP
-  update(state) {
+  update(state) { //ATUALIZA O ESTADO DE JOGO. 0 = START/INICIAR. 1 = PLAY/JOGAR. 2 = GAMEOVER/FIM DE JOGO.
     database.ref("/").update({
       gameState: state
     });
   }
-
   // TA
   start() {
     player = new Player();
     playerCount = player.getCount();
-
     form = new Form();
     form.display();
-
     car1 = createSprite(width / 2 - 50, height - 100);
     car1.addImage("car1", car1_img);
     car1.scale = 0.07;
-
     car2 = createSprite(width / 2 + 100, height - 100);
     car2.addImage("car2", car2_img);
     car2.scale = 0.07;
-
     cars = [car1, car2];
   }
 
@@ -46,29 +39,27 @@ class Game {
 
     Player.getPlayersInfo(); //added
 
-    if (allPlayers !== undefined) {
-      image(track, 0, -height * 5, width, height * 6);
-
-      //index of the array
+    if (allPlayers !== undefined) { //se todos os jogadores já estiverem definidos, irá exibir a pista e os carros
+      image(track, 0, -height * 5, width, height * 6); 
+      //Ao exibir a imagem, manteremos a posição x como 0 e y como (-height * 5); isso criará a pista fora da tela 
+      //porque não queremos mostrar a pista completa de uma vez, a pista será exibida conforme o jogador mover o carro.
+      //indice dos jogadores inicia em 0. O primeiro recebe o numero 1. O segundo recebe o numero 2.
       var index = 0;
       for (var plr in allPlayers) {
-        //use data form the database to display the cars in x and y direction
+        //plr é a variável usada para atravessar, e allPlayers é o objeto JavaScript no qual procuramos os valores
+        //ou seja, para realizar o posicionamento de todas as informaçoes de todos jogadores
         var x = allPlayers[plr].positionX;
         var y = height - allPlayers[plr].positionY;
-
         cars[index].position.x = x;
         cars[index].position.y = y;
-
-        //add 1 to the index for every loop
+        //adiciona um ao indice anterior
         index = index + 1;
       }
-
-      // handling keyboard events
+      //se a tecla pressionada para baixo é a SETA PARA CIMA  
       if (keyIsDown(UP_ARROW)) {
-        player.positionY += 10;
-        player.update();
+        player.positionY += 10; //aumente a posição do jogador de 10 em 10 a cada toque
+        player.update(); //atualize a posição no banco de dados
       }
- 
       drawSprites();
     }
   }
