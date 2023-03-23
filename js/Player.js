d4120922d57053764b17292fb4b790b1721404fb
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-03-23 17:50:32 -0300

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -1,28 +1,25 @@
 class Player {
   constructor() {
-    this.name = null;
-    this.index = null;
-    this.positionX = 0;
-    this.positionY = 0;
+    this.name = null; //esse nome é vazio (null) inicialmente
+    this.index = null; //esse indice (a quantia de jogadores registrada) é vazio inicialmente
+    this.positionX = 0; //a posição x do jogador é zerada inicialmente
+    this.positionY = 0; //a posição y do jogador é zerada inicialmente
   }
-
-   addPlayer() {
-    var playerIndex = "players/player" + this.index;
-
-    if (this.index === 1) {
+   addPlayer() { //adiciona o jogador
+    var playerIndex = "players/player" + this.index; //cria o indice/posição a partir das informações que temos
+    if (this.index === 1) { //se for o primeiro jogador a posição x será metade da largura subtraindo 100
       this.positionX = width / 2 - 100;
-    } else {
+    } else {//se for o segundo jogador a posição x será metade da largura adicionando 100
       this.positionX = width / 2 + 100;
     }
-
-    database.ref(playerIndex).set({
+    database.ref(playerIndex).set({ 
+      //no banco de dados irá trocar as informações conforme adicionadas no formulario e na função anterior
       name: this.name,
       positionX: this.positionX,
       positionY: this.positionY,
     });
   }
-  //TA
-  getDistance() {
+  getDistance() { //serve para obter/pegar a distancia do banco de dados e repassar aos jogadores
     var playerDistanceRef = database.ref("players/player" + this.index);
     playerDistanceRef.on("value", data => {
       var data = data.val();
@@ -30,24 +27,18 @@ class Player {
       this.positionY = data.positionY;
     });
   }
- 
-  //Bp
-  getCount() {
+  getCount() { //serve para obter/pegar a contagem de total de jogadores registrados no banco de dados
     var playerCountRef = database.ref("playerCount");
     playerCountRef.on("value", data => {
       playerCount = data.val();
     });
   }
-
-  //Bp
-  updateCount(count) {
+  updateCount(count) { //atualiza a contagem
     database.ref("/").update({
       playerCount: count
     });
   }
-
-  //SA
-  update() {
+  update() { //atualiza as informações no banco de dados sobre o jogador AULA C39
     var playerIndex = "players/player" + this.index;
     database.ref(playerIndex).update({
       name: this.name,
@@ -55,8 +46,8 @@ class Player {
       positionY: this.positionY,
      });
   }
-
-  //Bp
+  //As funções estáticas não são anexadas a nenhum objeto da classe. Estamos tentando obter todas as informações dos jogadores 
+  //— e isso não envolve nenhum objeto em particular. AULA C39
   static getPlayersInfo() {
     var playerInfoRef = database.ref("players");
     playerInfoRef.on("value", data => {
